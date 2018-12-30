dedfa30aedeeac5b1ccee593eb1bd1edbd74a319
Nicolá Domingues <nsdomingues44@gmail.com>
2018-12-30 12:13:54 +0000

    Reusing instantiation

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -2,10 +2,6 @@ package model.dao;
 
 import java.util.List;
 
-<<<<<<< HEAD
-=======
-import model.entities.Department;
->>>>>>> 185df995bf4c7920dfb883911180a1186a6d2e66
 import model.entities.Seller;
 
 public interface SellerDao {
@@ -14,11 +10,7 @@ public interface SellerDao {
 	void insert(Seller obj);
 	void update(Seller obj);
 	void deleteById(Integer id);
-<<<<<<< HEAD
 	Seller findById(Integer id);
-=======
-	Department findById(Integer id);
->>>>>>> 185df995bf4c7920dfb883911180a1186a6d2e66
 	List<Seller> findAll();
 
 
