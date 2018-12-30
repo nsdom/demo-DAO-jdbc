582cad0f6bc3c60ad2c93b2be847c18a8df4b9ab
Nicolá Domingues <nsdomingues44@gmail.com>
2018-12-30 13:43:33 +0000

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -39,6 +39,13 @@ public class Program {
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted new id = " + newSeller.getId());
 	
+System.out.println("\n==== TEST 5: seller update =======");
+		
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Updated new name = " + seller.getName());
+	
 	}
 	
 	
